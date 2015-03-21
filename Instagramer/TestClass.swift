faeb387e04386074956596d4ffb65e3c59b0be59
TakuroOnoda <takuro.onoda@gmail.com>
2015-03-21 16:09:03 +0900

    Instagramer:addとcommitを入れ替え

---
+++ Instagramer/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  Instagramer
+//
+//  Created by OnodaTakuro on 2015/03/21.
+//
+//
+
+import Foundation
+public class TestClass {
+    public class func putLog() {
+        NSLog("aa")
+    }
+}
